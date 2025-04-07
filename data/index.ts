9c093bad4cfeeed02fe7bd90320d40c506ca2068
Abhijith Krishna A K <138287385+Abhijith-AK@users.noreply.github.com>
2025-04-07 18:26:08 +0530

    final

---
+++ data/index.ts
@@ -0,0 +1,160 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+    {
+        id: 1,
+        title: "I prioritize client collaboration, fostering open communication ",
+        description: "",
+        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        imgClassName: "w-full h-full",
+        titleClassName: "justify-end",
+        img: "/b1.svg",
+        spareImg: "",
+    },
+    {
+        id: 2,
+        title: "I'm very flexible with time zone communications",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 3,
+        title: "My tech stack",
+        description: "I constantly try to improve",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-center",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 4,
+        title: "Tech enthusiast with a passion for development.",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "/grid.svg",
+        spareImg: "/b4.svg",
+    },
+
+    {
+        id: 5,
+        title: "Currently building a JS Animation library",
+        description: "The Inside Scoop",
+        className: "md:col-span-3 md:row-span-2",
+        imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+        titleClassName: "justify-center md:justify-start lg:justify-center",
+        img: "/b5.svg",
+        spareImg: "/grid.svg",
+    },
+    {
+        id: 6,
+        title: "Do you want to start a project together?",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+        img: "",
+        spareImg: "",
+    },
+];
+
+export const projects = [
+    {
+        id: 1,
+        title: "FlowCRM - Customizable CRM Platform",
+        des: "A lightweight, scalable CRM solution with custom workflows, lead tracking, analytics, and real-time communication, built for growing businesses.",
+        img: "/flowcrm.png",
+        iconLists: ["/re.svg", "/node.svg", "/express.svg", "/mongo.svg", "/socket.svg"],
+        link: "https://github.com/Abhijith-AK/flow-crm",
+    },
+    {
+        id: 2,
+        title: "My Web Diary ",
+        des: "A Online Diary Entry Web App made using React JS ",
+        img: "/diary.png",
+        iconLists: ["/re.svg", "/node.svg", "/tail.svg"],
+        link: "https://github.com/Abhijith-AK/my_diary",
+    },
+    {
+        id: 3,
+        title: "Skycast - Weather App",
+        des: "A fully functional Weather Dashboard Web App ",
+        img: "/skycast.png",
+        iconLists: ["/re.svg", "/tail.svg", "/open.svg"],
+        link: "https://github.com/Abhijith-AK/skycast",
+    },
+    {
+        id: 4,
+        title: "Animated Apple iMac Website",
+        des: "Recreated the Apple iMac website, combining GSAP animations and Tailwind effects..",
+        img: "/imac.png",
+        iconLists: ["/js.svg", "/tail.svg", "/html.svg", "/css.svg", "/gsap.svg"],
+        link: "https://github.com/Abhijith-AK/iMac-clone",
+    },
+];
+
+export const companies = [
+    {
+        id: 1,
+        name: "cloudinary",
+        img: "/cloud.svg",
+        nameImg: "/cloudName.svg",
+    },
+    {
+        id: 2,
+        name: "appwrite",
+        img: "/app.svg",
+        nameImg: "/appName.svg",
+    },
+    {
+        id: 3,
+        name: "HOSTINGER",
+        img: "/host.svg",
+        nameImg: "/hostName.svg",
+    },
+    {
+        id: 4,
+        name: "stream",
+        img: "/s.svg",
+        nameImg: "/streamName.svg",
+    },
+    {
+        id: 5,
+        name: "docker.",
+        img: "/dock.svg",
+        nameImg: "/dockerName.svg",
+    },
+];
+
+export const workExperience = [
+    {
+        id: 1,
+        title: "Full-Stack Development Intern (MERN/MEAN Stack)",
+        desc: "Contributed to building interactive and scalable full-stack web applications using React.js and Node.js, enhancing user experience and application performance.",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    }
+];
+
+export const socialMedia = [
+    {
+        id: 1,
+        img: "/git.svg",
+        link: 'https://github.com/Abhijith-AK'
+    },
+    {
+        id: 2,
+        img: "/link.svg",
+        link: 'https://www.linkedin.com/in/abhijith-krishna-a-k'
+    },
+];
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5c4f92982e0eaa8825de17e388f2da83@o4509096103444480.ingest.us.sentry.io/4509096105213952",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
